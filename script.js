49d569a9c17629ab895e17f34d430b72dbd91dfe
jamesjavierm <101679227+jamesjavierm@users.noreply.github.com>
2024-09-25 21:22:50 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const rootDiv = document.getElementById('root');
 
+function renderSignUp() {
+    rootDiv.innerHTML = `
+        <h1>Sign Up</h1>
+        <form id="signupForm">
+            <label for="name">Name:</label>
+            <input type="text" id="name" placeholder="Enter your name"><br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" placeholder="Enter your email"><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" placeholder="Enter your password"><br>
+            <button type="button" onclick="handleSignUp()">Sign Up</button>
+        </form>
+    ;
+}
